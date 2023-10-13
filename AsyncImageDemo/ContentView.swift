d838dc45e8c0375c04d3d08e41009c8796c8ca09
ainame <s.namai.09@gmail.com>
2023-10-13 15:37:04 +0100

    Add demo code for feedback

--- AsyncImageDemo/ContentView.swift
+++ AsyncImageDemo/ContentView.swift
@@ -7,15 +7,54 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ItemView: View {
+    let url: URL?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        AsyncImage(url: url) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+            case .success(let image):
+                image
+                    .aspectRatio(contentMode: .fill)
+                    .containerRelativeFrame([.vertical, .horizontal])
+                    .clipped()
+            case .failure(let error):
+                let _ = print(error)
+                Color.red
+                    .containerRelativeFrame([.vertical, .horizontal])
+                    .clipped()
+            @unknown default:
+                ProgressView()
+            }
         }
-        .padding()
+    }
+
+}
+
+struct ContentView: View {
+    let urls: [URL?] = [
+        URL(string: "https://picsum.photos/900/1600?0"),
+        URL(string: "https://picsum.photos/900/1600?1"),
+        URL(string: "https://picsum.photos/900/1600?2"),
+        URL(string: "https://picsum.photos/900/1600?3"),
+        URL(string: "https://picsum.photos/900/1600?4"),
+    ]
+
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 0) {
+                ForEach(urls, id: \.self) { url in
+                    ItemView(url: url)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .containerRelativeFrame([.horizontal, .vertical])
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .ignoresSafeArea()
+        .scrollTargetBehavior(.paging)
     }
 }
 
