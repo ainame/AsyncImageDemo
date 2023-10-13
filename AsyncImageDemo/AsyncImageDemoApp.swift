36ba6523f317f4146fdb3e5cb722f6808ff329e8
ainame <s.namai.09@gmail.com>
2023-10-13 15:17:41 +0100

    Initial Commit

---
+++ AsyncImageDemo/AsyncImageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageDemoApp.swift
+//  AsyncImageDemo
+//
+//  Created by Satoshi Namai on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
